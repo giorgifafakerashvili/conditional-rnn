2a2f4455807a07b2d0a390cc774e6ecdaec521c7
= <giorgi.fafa@gmail.com>
2017-06-12 19:39:32 +0400

    added Utils.h

---
+++ char-rnn-model/Utils.h
@@ -0,0 +1,15 @@
+#ifndef _UTILS_H_
+#define _UTILS_H_
+
+#include <stdlib.h>
+#include "Vector.h"
+
+// Declaration of Vector class
+class Vector;
+
+double uniRand();
+double randn();
+
+int sampleFromVector(Vector&); 
+
+#endif // _UTILS_H_
