7f6c87c17cbfbe3781cc5082314a2158ec61a46f
= <giorgi.fafa@gmail.com>
2017-06-12 19:41:37 +0400

    added matrix.h partially

---
+++ char-rnn-model/Matrix.h
@@ -0,0 +1,16 @@
+#ifndef _MATRIX_H_
+#define _MATRIX_H_
+
+#include "Utils.h"
+#include <stdio.h>
+#include <assert.h>
+#include <fstream>
+
+// Declaration of Vector
+class Vector;
+
+class Matrix {
+
+};
+
+#endif // _MATRIX_H_
